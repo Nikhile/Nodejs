50e1f7767ff70df15a3609b0164a6c85182ed8d0
Nikhile <nikhilelpula@gmail.com>
2017-04-27 20:01:01 -0500

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,9 @@
+ar sum=require('./arithmetic');
+var mul=require('./arithmetic');
+var sub=require('./arithmetic');
+var div=require('./arithmetic');
+
+console.log(sum.add(5,7));
+console.log(sum.mul(5,7));
+console.log(sum.sub(5,7));
+console.log(sum.div(5,7));
